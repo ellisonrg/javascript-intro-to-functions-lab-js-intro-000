9846f2cdbbbddc19c28832cae17672e404911551
Reed G <ellisonrg@gmail.com>
2019-02-28 19:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-    if ('hello' === "lowercase"){
+    if (string.toLowerCase() === string){
       return "I can\'t hear you!"
-} else if ("HELLO" === "uppercase"){
+} else if (string.toUpperCase() === string){
       return "YES INDEED!"
-} else if ("I love you, Grandma." === "I love you, Grandma."){
+} else if (string === "I love you, Grandma."){
       return "I love you, too."
 }
 }
